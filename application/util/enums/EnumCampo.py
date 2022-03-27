d15a88a7f4fcd41b06602356ef327389b199b320
AlisonBarreiro <alison.rmb@gmail.com>
2022-03-27 12:01:50 -0300

    Renamed to EnumCampo

--- application/util/enums/EnumCampo.py
+++ application/util/enums/EnumCampo.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
 
-class Campo(Enum):
+class EnumCampo(Enum):
     largura = 68
     larguraMin = 45
     larguraMax = 90
@@ -39,26 +39,3 @@ class Campo(Enum):
 
     bandeiraAltura = 1.5
     bandeiraAlturaMeio = 1
-
-
-class Bola(Enum):
-    # circunferência em metros
-    circunferencia = 0.69
-    circunferênciaMin = 0.68
-    circunferênciaMax = 0.70
-    # peso em gramas
-    peso = 430
-    pesoMin = 410
-    pesoMax = 450
-    # pressão atmosferas (600 – 1100 g/cm2) ao nível do mar
-    psi = 0.6
-    psiMin = 0.6
-    psiMax = 1.1
-
-    ####### Padrão aprovado pela FIFA  ###########
-    # Teste de rebote uniforme <= 10cm
-    # Perda de pressão <= 20%
-    # Absorção de água <= 10%  de aumento de peso
-    # Diâmetro  1,5% de diferença
-
-
